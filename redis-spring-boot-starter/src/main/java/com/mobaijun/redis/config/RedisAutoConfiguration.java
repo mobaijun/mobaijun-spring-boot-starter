5ae4bbd03cbc3bf4cdc321379e6ee70a934c2b07
mobaijun <mobaijun8@163.com>
2022-04-28 16:45:41 +0800

    @modify update redis starter module

---
+++ redis-spring-boot-starter/src/main/java/com/mobaijun/redis/config/RedisAutoConfiguration.java
@@ -0,0 +1,71 @@
+package com.mobaijun.redis.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mobaijun.redis.prop.RedisProperties;
+import com.mobaijun.redis.util.RedisLockUtil;
+import com.mobaijun.redis.util.RedisUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * Software：IntelliJ IDEA 2021.3.2
+ * ClassName: RedisAutoConfiguration
+ * 类描述： Redis 配置类
+ *
+ * @author MoBaiJun 2022/4/28 15:52
+ */
+@Configuration
+@ConditionalOnProperty(name = RedisProperties.PREFIX + ".enable", havingValue = "true", matchIfMissing = true)
+public class RedisAutoConfiguration {
+
+    @Bean
+    public RedisSerializer<String> redisKeySerializer() {
+        return RedisSerializer.string();
+    }
+
+    @Bean
+    public RedisSerializer<Object> redisValueSerializer() {
+        return RedisSerializer.json();
+    }
+
+    @SuppressWarnings("all")
+    @Bean(name = "redisTemplate")
+    @ConditionalOnClass(RedisOperations.class)
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        return redisTemplate;
+    }
+
+    @Bean
+    @ConditionalOnBean(name = "redisTemplate")
+    public RedisUtils redisService() {
+        return new RedisUtils();
+    }
+
+    @Bean
+    @ConditionalOnBean(name = "redisTemplate")
+    public RedisLockUtil redisLockUtil(RedisTemplate<String, Object> redisTemplate) {
+        return new RedisLockUtil(redisTemplate);
+    }
+}
---
+++ redis-spring-boot-starter/src/main/java/com/mobaijun/redis/util/RedisLockUtil.java
@@ -0,0 +1,37 @@
+package com.mobaijun.redis.util;
+
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Software：IntelliJ IDEA 2021.3.2
+ * ClassName: RedisLockUtil
+ * 类描述： Redis 锁
+ *
+ * @author MoBaiJun 2022/4/28 16:06
+ */
+@Component
+public class RedisLockUtil {
+
+    public RedisLockUtil(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final byte[] SCRIPT_RELEASE_LOCK = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end".getBytes();
+
+    public synchronized Boolean tryLock(String key, String requestId, long expire) {
+        return redisTemplate.execute((RedisCallback<Boolean>) redisConnection -> redisConnection.set(key.getBytes(), requestId.getBytes(), Expiration.from(expire, TimeUnit.SECONDS), RedisStringCommands.SetOption.SET_IF_ABSENT));
+    }
+
+    public synchronized void releaseLock(String key, String requestId) {
+        redisTemplate.execute((RedisCallback<Boolean>) redisConnection -> redisConnection.eval(SCRIPT_RELEASE_LOCK, ReturnType.BOOLEAN, 1, key.getBytes(), requestId.getBytes()));
+    }
+}
