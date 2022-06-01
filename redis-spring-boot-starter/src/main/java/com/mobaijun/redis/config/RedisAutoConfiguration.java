ded53eee6429dc25a8c2b275d600628410cf661a
mobaijun <mobaijun8@163.com>
2022-06-01 15:25:04 +0800

    @modify update redis 序列化异常

--- redis-spring-boot-starter/src/main/java/com/mobaijun/redis/config/RedisAutoConfiguration.java
+++ redis-spring-boot-starter/src/main/java/com/mobaijun/redis/config/RedisAutoConfiguration.java
@@ -68,8 +68,8 @@ public class RedisAutoConfiguration {
      * @return RedisTemplate RedisTemplate
      */
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
         Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
--- redis-spring-boot-starter/src/main/java/com/mobaijun/redis/constant/RedisConstant.java
+++ redis-spring-boot-starter/src/main/java/com/mobaijun/redis/constant/RedisConstant.java
@@ -1,5 +1,7 @@
 package com.mobaijun.redis.constant;
 
+import java.time.Duration;
+
 /**
  * software：IntelliJ IDEA 2022.1
  * class name: RedisConstant
@@ -18,4 +20,29 @@ public class RedisConstant {
      * 默认缓存过期时间 1 天
      */
     public static final Long DAY = 1L;
+
+    /**
+     * 24小时
+     */
+    public static final Duration ONE_DAY = Duration.ofDays(1);
+
+    /**
+     * 一周(7天)
+     */
+    public static final Duration WEEK_DAY = Duration.ofDays(7);
+
+    /**
+     * 30 分钟
+     */
+    public static final Duration THREE_MINUTES = Duration.ofMinutes(30);
+
+    /**
+     * 60 分钟
+     */
+    public static final Duration SIXTS_MINUTES = Duration.ofMinutes(60);
+
+    /**
+     * 60 秒
+     */
+    public static final Duration SIXTS_SECONDS = Duration.ofSeconds(60);
 }
