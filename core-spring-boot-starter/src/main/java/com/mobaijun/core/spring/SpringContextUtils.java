7e319225d3dd3c9ba4d4bd1b829679afc24ebbcd
mobaijun <mobaijun8@163.com>
2024-07-30 18:36:40 +0800

    :bug: spring 工具类扩展

--- core-spring-boot-starter/src/main/java/com/mobaijun/core/spring/SpringContextUtils.java
+++ core-spring-boot-starter/src/main/java/com/mobaijun/core/spring/SpringContextUtils.java
@@ -1,17 +1,24 @@
 package com.mobaijun.core.spring;
 
+import cn.hutool.core.lang.TypeReference;
+import cn.hutool.core.util.ArrayUtil;
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
-import org.springframework.aop.framework.AopContext;
+import java.util.Map;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.autoconfigure.thread.Threading;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.ResolvableType;
 import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
-import org.springframework.util.StringUtils;
 
 /**
  * software：IntelliJ IDEA 2022.2.3<br>
@@ -45,98 +52,291 @@ public class SpringContextUtils implements ApplicationContextAware, BeanFactoryP
     }
 
     /**
-     * getApplicationContext
+     * 获取{@link ApplicationContext}
      *
-     * @return ApplicationContext
+     * @return {@link ApplicationContext}
      */
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
 
     /**
-     * getBean
+     * 获取{@link org.springframework.beans.factory.ListableBeanFactory}，可能为{@link ConfigurableListableBeanFactory} 或 {@link ApplicationContextAware}
      *
-     * @param name service 注解方式 name 为小驼峰格式
-     * @return Object bean 的实例对象
+     * @return {@link org.springframework.beans.factory.ListableBeanFactory}
+     */
+    public static ListableBeanFactory getBeanFactory() {
+        final ListableBeanFactory factory = null == beanFactory ? applicationContext : beanFactory;
+        if (null == factory) {
+            throw new RuntimeException("No ConfigurableListableBeanFactory or ApplicationContext injected, maybe not in the Spring environment?");
+        }
+        return factory;
+    }
+
+    /**
+     * 获取{@link ConfigurableListableBeanFactory}
+     *
+     * @return {@link ConfigurableListableBeanFactory}
+     */
+    public static ConfigurableListableBeanFactory getConfigurableBeanFactory() {
+        final ConfigurableListableBeanFactory factory;
+        if (null != beanFactory) {
+            factory = beanFactory;
+        } else if (applicationContext instanceof ConfigurableApplicationContext) {
+            factory = ((ConfigurableApplicationContext) applicationContext).getBeanFactory();
+        } else {
+            throw new RuntimeException("No ConfigurableListableBeanFactory from context!");
+        }
+        return factory;
+    }
+
+    /**
+     * 通过name获取 Bean
+     *
+     * @param <T>  Bean类型
+     * @param name Bean名称
+     * @return Bean
      */
     @SuppressWarnings("unchecked")
-    public static <T> T getBean(String name) throws BeansException {
-        return (T) applicationContext.getBean(name);
+    public static <T> T getBean(String name) {
+        return (T) getBeanFactory().getBean(name);
     }
 
     /**
-     * getBean
+     * 通过class获取Bean
      *
-     * @param clz service 对应的类
-     * @return Object bean 的实例对象
+     * @param <T>   Bean类型
+     * @param clazz Bean类
+     * @return Bean对象
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getBeanFactory().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param <T>   bean类型
+     * @param name  Bean名称
+     * @param clazz bean类型
+     * @return Bean对象
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getBeanFactory().getBean(name, clazz);
+    }
+
+    /**
+     * 通过类型参考返回带泛型参数的Bean
+     *
+     * @param reference 类型参考，用于持有转换后的泛型类型
+     * @param <T>       Bean类型
+     * @return 带泛型参数的Bean
+     * @since 5.4.0
      */
     @SuppressWarnings("unchecked")
-    public static <T> T getBean(Class<?> clz) throws BeansException {
-        return (T) applicationContext.getBean(clz);
+    public static <T> T getBean(TypeReference<T> reference) {
+        final ParameterizedType parameterizedType = (ParameterizedType) reference.getType();
+        final Class<T> rawType = (Class<T>) parameterizedType.getRawType();
+        final Class<?>[] genericTypes = Arrays.stream(parameterizedType.getActualTypeArguments()).map(type -> (Class<?>) type).toArray(Class[]::new);
+        final String[] beanNames = getBeanFactory().getBeanNamesForType(ResolvableType.forClassWithGenerics(rawType, genericTypes));
+        return getBean(beanNames[0], rawType);
     }
 
     /**
-     * 如果 BeanFactory 包含一个与所给名称匹配的 bean 定义，则返回 true
+     * 获取指定类型对应的所有Bean，包括子类
      *
-     * @return boolean
+     * @param <T>  Bean类型
+     * @param type 类、接口，null表示获取所有bean
+     * @return 类型对应的bean，key是bean注册的name，value是Bean
      */
-    public static boolean containsBean(String name) {
-        return beanFactory.containsBean(name);
+    public static <T> Map<String, T> getBeansOfType(Class<T> type) {
+        return getBeanFactory().getBeansOfType(type);
     }
 
     /**
-     * 判断以给定名字注册的bean定义是一个singleton还是一个prototype。
-     * 如果与给定名字相应的bean定义没有被找到，将会抛出一个异常（NoSuchBeanDefinitionException）
+     * 获取指定类型对应的Bean名称，包括子类
      *
-     * @return boolean
+     * @param type 类、接口，null表示获取所有bean名称
+     * @return bean名称
      */
-    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
-        return beanFactory.isSingleton(name);
+    public static String[] getBeanNamesForType(Class<?> type) {
+        return getBeanFactory().getBeanNamesForType(type);
     }
 
     /**
-     * getType
+     * 获取配置文件配置项的值
      *
-     * @return Class 注册对象的类型
+     * @param key 配置项key
+     * @return 属性值
      */
-    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
-        return beanFactory.getType(name);
+    public static String getProperty(String key) {
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getProperty(key);
     }
 
     /**
-     * 如果给定的bean名字在bean定义中有别名，则返回这些别名
-     */
-    public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
-        return beanFactory.getAliases(name);
-    }
-
-    /**
-     * 获取 aop 代理对象
+     * 获取配置文件配置项的值
      *
-     * @param invoker invoker
+     * @param key          配置项key
+     * @param defaultValue 默认值
+     * @return 属性值
      */
-    @SuppressWarnings("unchecked")
-    public static <T> T getAopProxy(T invoker) {
-        return (T) AopContext.currentProxy();
+    public static String getProperty(String key, String defaultValue) {
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getProperty(key, defaultValue);
     }
 
     /**
-     * 获取当前的环境配置，当有多个环境配置时，只获取第一个
+     * 获取配置文件配置项的值
      *
-     * @return Current environment configuration
+     * @param <T>          属性值类型
+     * @param key          配置项key
+     * @param targetType   配置项类型
+     * @param defaultValue 默认值
+     * @return 属性值
+     */
+    public static <T> T getProperty(String key, Class<T> targetType, T defaultValue) {
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getProperty(key, targetType, defaultValue);
+    }
+
+    /**
+     * 获取应用程序名称
+     *
+     * @return 应用程序名称
+     */
+    public static String getApplicationName() {
+        return getProperty("spring.application.name");
+    }
+
+    /**
+     * 获取当前的环境配置，无配置返回null
+     *
+     * @return 当前的环境配置
      */
     public static String[] getActiveProfiles() {
+        if (null == applicationContext) {
+            return null;
+        }
         return applicationContext.getEnvironment().getActiveProfiles();
     }
 
     /**
      * 获取当前的环境配置，当有多个环境配置时，只获取第一个
      *
-     * @return Current environment configuration
+     * @return 当前的环境配置
      */
     public static String getActiveProfile() {
         final String[] activeProfiles = getActiveProfiles();
-        return !StringUtils.hasText(Arrays.toString(activeProfiles)) ? activeProfiles[0] : null;
+        return ArrayUtil.isNotEmpty(activeProfiles) ? activeProfiles[0] : null;
+    }
+
+    /**
+     * 动态向Spring注册Bean
+     * <p>
+     * 由{@link org.springframework.beans.factory.BeanFactory} 实现，通过工具开放API
+     * <p>
+     * 更新: shadow 2021-07-29 17:20:44 增加自动注入，修复注册bean无法反向注入的问题
+     *
+     * @param <T>      Bean类型
+     * @param beanName 名称
+     * @param bean     bean
+     * @author shadow
+     */
+    public static <T> void registerBean(String beanName, T bean) {
+        final ConfigurableListableBeanFactory factory = getConfigurableBeanFactory();
+        factory.autowireBean(bean);
+        factory.registerSingleton(beanName, bean);
+    }
+
+    /**
+     * 注销bean
+     * <p>
+     * 将Spring中的bean注销，请谨慎使用
+     *
+     * @param beanName bean名称
+     * @author shadow
+     */
+    public static void unregisterBean(String beanName) {
+        final ConfigurableListableBeanFactory factory = getConfigurableBeanFactory();
+        if (factory instanceof DefaultSingletonBeanRegistry registry) {
+            registry.destroySingleton(beanName);
+        } else {
+            throw new RuntimeException("Can not unregister bean, the factory is not a DefaultSingletonBeanRegistry!");
+        }
+    }
+
+    /**
+     * 发布事件
+     *
+     * @param event 待发布的事件，事件必须是{@link ApplicationEvent}的子类
+     */
+    public static void publishEvent(ApplicationEvent event) {
+        if (null != applicationContext) {
+            applicationContext.publishEvent(event);
+        }
+    }
+
+    /**
+     * 发布事件
+     * Spring 4.2+ 版本事件可以不再是{@link ApplicationEvent}的子类
+     *
+     * @param event 待发布的事件
+     */
+    public static void publishEvent(Object event) {
+        if (null != applicationContext) {
+            applicationContext.publishEvent(event);
+        }
+    }
+
+    /**
+     * 如果BeanFactory包含一个与所给名称匹配的bean定义，则返回true
+     */
+    public static boolean containsBean(String name) {
+        return getBeanFactory().containsBean(name);
+    }
+
+    /**
+     * 判断以给定名字注册的bean定义是一个singleton还是一个prototype。
+     * 如果与给定名字相应的bean定义没有被找到，将会抛出一个异常（NoSuchBeanDefinitionException）
+     */
+    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+        return getBeanFactory().isSingleton(name);
+    }
+
+    /**
+     * @return Class 注册对象的类型
+     */
+    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
+        return getBeanFactory().getType(name);
+    }
+
+    /**
+     * 如果给定的bean名字在bean定义中有别名，则返回这些别名
+     */
+    public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
+        return getBeanFactory().getAliases(name);
+    }
+
+    /**
+     * 获取aop代理对象
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getAopProxy(T invoker) {
+        return (T) getBean(invoker.getClass());
+    }
+
+    /**
+     * 获取spring上下文
+     */
+    public static ApplicationContext context() {
+        return getApplicationContext();
     }
 
     public static boolean isVirtual() {
