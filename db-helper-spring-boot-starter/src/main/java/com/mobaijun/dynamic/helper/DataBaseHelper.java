29954460ff7ca839c4ceb192d2ef9ce4db7a7807
mobaijun <mobaijun8@163.com>
2024-08-13 14:49:44 +0800

    :art: 添加数据库操作助手工具类包

---
+++ db-helper-spring-boot-starter/src/main/java/com/mobaijun/dynamic/helper/DataBaseHelper.java
@@ -0,0 +1,107 @@
+package com.mobaijun.dynamic.helper;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.mobaijun.core.spring.SpringUtil;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+
+/**
+ * Description: [动态数据源工具类，提供数据库类型判断和数据源名称获取的功能。]
+ * Author: [mobaijun]
+ * Date: [2024/8/13 14:13]
+ * IntelliJ IDEA Version: [IntelliJ IDEA 2023.1.4]
+ */
+public class DataBaseHelper {
+
+    private static final DynamicRoutingDataSource DS = SpringUtil.getBean(DynamicRoutingDataSource.class);
+
+    /**
+     * 获取当前数据库类型。
+     *
+     * @return 当前数据库类型
+     */
+    public static DbType getDataBaseType() {
+        DataSource dataSource = DS.determineDataSource();
+        try (Connection conn = dataSource.getConnection()) {
+            DatabaseMetaData metaData = conn.getMetaData();
+            String databaseProductName = metaData.getDatabaseProductName();
+            return DbType.getDbType(databaseProductName);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * 判断当前数据库是否为MySQL。
+     *
+     * @return 如果是MySQL则返回true，否则返回false
+     */
+    public static boolean isMySql() {
+        return DbType.MYSQL == getDataBaseType();
+    }
+
+    /**
+     * 判断当前数据库是否为Oracle。
+     *
+     * @return 如果是Oracle则返回true，否则返回false
+     */
+    public static boolean isOracle() {
+        return DbType.ORACLE == getDataBaseType();
+    }
+
+    /**
+     * 判断当前数据库是否为PostgreSQL。
+     *
+     * @return 如果是PostgreSQL则返回true，否则返回false
+     */
+    public static boolean isPostgerSql() {
+        return DbType.POSTGRE_SQL == getDataBaseType();
+    }
+
+    /**
+     * 判断当前数据库是否为SQL Server。
+     *
+     * @return 如果是SQL Server则返回true，否则返回false
+     */
+    public static boolean isSqlServer() {
+        return DbType.SQL_SERVER == getDataBaseType();
+    }
+
+    /**
+     * 根据数据库类型，生成对应的SQL查找语句。
+     *
+     * @param args 要查找的值
+     * @param var2 要查找的字符串
+     * @return 生成的SQL查找语句
+     */
+    public static <T> String findInSet(T args, String var2) {
+        DbType dbType = getDataBaseType();
+        String var = (String) args;
+        if (dbType == DbType.SQL_SERVER) {
+            // char index(',100,' , ',0,100,101,') <> 0
+            return "charindex(',%s,' , ','+%s+',') <> 0".formatted(var, var2);
+        } else if (dbType == DbType.POSTGRE_SQL) {
+            // (select position(',100,' in ',0,100,101,')) <> 0
+            return "(select position(',%s,' in ','||%s||',')) <> 0".formatted(var, var2);
+        } else if (dbType == DbType.ORACLE) {
+            // instr(',0,100,101,' , ',100,') <> 0
+            return "instr(','||%s||',' , ',%s,') <> 0".formatted(var2, var);
+        }
+        // find_in_set('100' , '0,100,101')
+        return "find_in_set('%s' , %s) <> 0".formatted(var, var2);
+    }
+
+    /**
+     * 获取当前加载的所有数据源名称列表。
+     *
+     * @return 数据源名称列表
+     */
+    public static List<String> getDataSourceNameList() {
+        return new ArrayList<>(DS.getDataSources().keySet());
+    }
+}
---
+++ db-helper-spring-boot-starter/src/main/java/com/mobaijun/dynamic/helper/DataPermissionHelper.java
@@ -0,0 +1,108 @@
+package com.mobaijun.dynamic.helper;
+
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.context.model.SaStorage;
+import com.baomidou.mybatisplus.core.plugins.IgnoreStrategy;
+import com.baomidou.mybatisplus.core.plugins.InterceptorIgnoreHelper;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Description: [数据权限助手]
+ * Author: [mobaijun]
+ * Date: [2024/8/13 14:34]
+ * IntelliJ IDEA Version: [IntelliJ IDEA 2023.1.4]
+ */
+@SuppressWarnings("unchecked cast")
+public class DataPermissionHelper {
+
+    public static final String DATA_PERMISSION_KEY = "data:permission";
+
+    /**
+     * 从上下文中获取指定键的变量值，并将其转换为指定的类型
+     *
+     * @param key 变量的键
+     * @param <T> 变量值的类型
+     * @return 指定键的变量值，如果不存在则返回 null
+     */
+    public static <T> T getVariable(String key) {
+        Map<String, Object> context = getContext();
+        return (T) context.get(key);
+    }
+
+    /**
+     * 向上下文中设置指定键的变量值
+     *
+     * @param key   要设置的变量的键
+     * @param value 要设置的变量值
+     */
+    public static void setVariable(String key, Object value) {
+        Map<String, Object> context = getContext();
+        context.put(key, value);
+    }
+
+    /**
+     * 获取数据权限上下文
+     *
+     * @return 存储在SaStorage中的Map对象，用于存储数据权限相关的上下文信息
+     * @throws NullPointerException 如果数据权限上下文类型异常，则抛出NullPointerException
+     */
+    public static Map<String, Object> getContext() {
+        SaStorage saStorage = SaHolder.getStorage();
+        Object attribute = saStorage.get(DATA_PERMISSION_KEY);
+        if (Objects.isNull(attribute)) {
+            saStorage.set(DATA_PERMISSION_KEY, new HashMap<>());
+            attribute = saStorage.get(DATA_PERMISSION_KEY);
+        }
+        if (attribute instanceof Map map) {
+            return map;
+        }
+        throw new NullPointerException("data permission context type exception");
+    }
+
+    /**
+     * 开启忽略数据权限(开启后需手动调用 {@link #disableIgnore()} 关闭)
+     */
+    public static void enableIgnore() {
+        InterceptorIgnoreHelper.handle(IgnoreStrategy.builder().dataPermission(true).build());
+    }
+
+    /**
+     * 关闭忽略数据权限
+     */
+    public static void disableIgnore() {
+        InterceptorIgnoreHelper.clearIgnoreStrategy();
+    }
+
+    /**
+     * 在忽略数据权限中执行
+     * <p>禁止在忽略数据权限中执行忽略数据权限</p>
+     *
+     * @param handle 处理执行方法
+     */
+    public static void ignore(Runnable handle) {
+        enableIgnore();
+        try {
+            handle.run();
+        } finally {
+            disableIgnore();
+        }
+    }
+
+    /**
+     * 在忽略数据权限中执行
+     * <p>禁止在忽略数据权限中执行忽略数据权限</p>
+     *
+     * @param handle 处理执行方法
+     */
+    public static <T> T ignore(Supplier<T> handle) {
+        enableIgnore();
+        try {
+            return handle.get();
+        } finally {
+            disableIgnore();
+        }
+    }
+}
