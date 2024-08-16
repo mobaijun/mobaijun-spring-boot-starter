9a50e52393fbe37e0d1e5b1a606694328539a74e
mobaijun <mobaijun8@163.com>
2024-08-16 09:36:28 +0800

    :bug: 处理spring boot3 请求图标异常

--- web-spring-boot-starter/src/main/java/com/mobaijun/web/config/ResourcesConfig.java
+++ web-spring-boot-starter/src/main/java/com/mobaijun/web/config/ResourcesConfig.java
@@ -15,10 +15,15 @@
  */
 package com.mobaijun.web.config;
 
+import com.mobaijun.common.enums.http.HttpStatus;
 import com.mobaijun.web.handler.GlobalExceptionHandler;
 import io.micrometer.common.lang.NonNullApi;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -35,7 +40,18 @@ public class ResourcesConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(@NotNull HttpServletRequest request,
+                                     @NotNull HttpServletResponse response, @NotNull Object handler) {
+                if (!"GET".equalsIgnoreCase(request.getMethod()) || !request.getRequestURI().equals("/favicon.ico")) {
+                    return true;
+                }
+                // 设置状态码为204 No Content
+                response.setStatus(HttpStatus.NO_CONTENT.getCode());
+                return false;
+            }
+        }).addPathPatterns("/**");
     }
 
     @Override
