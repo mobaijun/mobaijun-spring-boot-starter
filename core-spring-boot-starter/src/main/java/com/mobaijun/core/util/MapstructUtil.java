7abe428a2bf740dabde693c2c4e0ca7b5dc564fe
mobaijun <mobaijun8@163.com>
2024-08-19 16:20:45 +0800

    :bug: 添加类型转换器工具类

---
+++ core-spring-boot-starter/src/main/java/com/mobaijun/core/util/MapstructUtil.java
@@ -0,0 +1,91 @@
+package com.mobaijun.core.util;
+
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.mobaijun.core.spring.SpringUtil;
+import io.github.linpeilie.Converter;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Description:
+ * Mapstruct 工具类
+ * <p>参考文档：<a href="https://mapstruct.plus/introduction/quick-start.html">mapstruct-plus</a></p>
+ * Author: [mobaijun]
+ * Date: [2024/8/19 16:00]
+ * IntelliJ IDEA Version: [IntelliJ IDEA 2023.1.4]
+ */
+public class MapstructUtil {
+
+    private final static Converter CONVERTER = SpringUtil.getBean(Converter.class);
+
+    /**
+     * 将 T 类型对象，转换为 desc 类型的对象并返回
+     *
+     * @param source 数据来源实体
+     * @param desc   描述对象 转换后的对象
+     * @return desc
+     */
+    public static <T, V> V convert(T source, Class<V> desc) {
+        if (Objects.isNull(source)) {
+            return null;
+        }
+        if (Objects.isNull(desc)) {
+            return null;
+        }
+        return CONVERTER.convert(source, desc);
+    }
+
+    /**
+     * 将 T 类型对象，按照配置的映射字段规则，给 desc 类型的对象赋值并返回 desc 对象
+     *
+     * @param source 数据来源实体
+     * @param desc   转换后的对象
+     * @return desc
+     */
+    public static <T, V> V convert(T source, V desc) {
+        if (Objects.isNull(source)) {
+            return null;
+        }
+        if (Objects.isNull(desc)) {
+            return null;
+        }
+        return CONVERTER.convert(source, desc);
+    }
+
+    /**
+     * 将 T 类型的集合，转换为 desc 类型的集合并返回
+     *
+     * @param sourceList 数据来源实体列表
+     * @param desc       描述对象 转换后的对象
+     * @return desc
+     */
+    public static <T, V> List<V> convert(List<T> sourceList, Class<V> desc) {
+        if (sourceList.isEmpty()) {
+            return Collections.emptyList();
+        }
+        if (Objects.isNull(desc)) {
+            return null;
+        }
+        return CONVERTER.convert(sourceList, desc);
+    }
+
+    /**
+     * 将 Map 转换为 beanClass 类型的集合并返回
+     *
+     * @param map       数据来源
+     * @param beanClass bean类
+     * @return bean对象
+     */
+    public static <T> T convert(Map<String, Object> map, Class<T> beanClass) {
+        if (MapUtil.isEmpty(map)) {
+            return null;
+        }
+        if (ObjectUtil.isNull(beanClass)) {
+            return null;
+        }
+        return CONVERTER.convert(map, beanClass);
+    }
+}
