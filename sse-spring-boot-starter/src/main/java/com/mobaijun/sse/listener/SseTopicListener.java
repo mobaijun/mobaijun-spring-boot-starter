b0d50e3481c65d7dd7cd5126293028322de56703
mobaijun <mobaijun8@163.com>
2024-10-24 15:50:49 +0800

    :sparkles: sse 空指针修复

--- sse-spring-boot-starter/src/main/java/com/mobaijun/sse/listener/SseTopicListener.java
+++ sse-spring-boot-starter/src/main/java/com/mobaijun/sse/listener/SseTopicListener.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright (C) 2022 [www.mobaijun.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mobaijun.sse.listener;
 
 import com.mobaijun.sse.core.SseEmitterManager;
+import com.mobaijun.sse.message.SseMessage;
 import jakarta.annotation.Resource;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -28,21 +45,34 @@ public class SseTopicListener implements ApplicationRunner, Ordered {
      * 在Spring Boot应用程序启动时初始化SSE主题订阅监听器
      *
      * @param args 应用程序参数
-     * @throws Exception 初始化过程中可能抛出的异常
      */
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        sseEmitterManager.subscribeMessage((message) -> {
-            log.info("SSE主题订阅收到消息session keys={} message={}", message.getUserIds(), message.getMessage());
-            // 如果key不为空就按照key发消息 如果为空就群发
-            if (!message.getUserIds().isEmpty()) {
-                message.getUserIds().forEach(key -> sseEmitterManager.sendMessage(key, message.getMessage()));
-            } else {
-                sseEmitterManager.sendMessage(message.getMessage());
-            }
+    public void run(ApplicationArguments args) {
+        sseEmitterManager.subscribeMessage(message -> {
+            log.info("SSE主题订阅收到消息session keys={} message={}",
+                    message.getUserIds(), message.getMessage());
+            sendMessageToUsers(message);
         });
+
         log.info("初始化SSE主题订阅监听器成功");
     }
+
+    /**
+     * 向用户发送消息
+     *
+     * @param message 消息
+     */
+    private void sendMessageToUsers(SseMessage message) {
+        List<Long> userIds = message.getUserIds();
+        String msgContent = message.getMessage();
+
+        if (userIds != null && !userIds.isEmpty()) {
+            userIds.forEach(userId -> sseEmitterManager.sendMessage(userId, msgContent));
+        } else {
+            sseEmitterManager.sendMessage(msgContent);
+        }
+    }
+
 
     @Override
     public int getOrder() {
